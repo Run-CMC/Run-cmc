fe8ebc6ebd65e8294f043d9dab6bf997e9c6dcaa
Orenthal Hill <ojhill98@gmail.com>
2021-11-18 15:27:23 -0600

    initial commit

---
+++ src/main/java/com/codeup/runcmc/RunCmcApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.runcmc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunCmcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RunCmcApplication.class, args);
+    }
+
+}
