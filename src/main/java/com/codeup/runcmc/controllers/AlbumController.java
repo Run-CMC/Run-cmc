0b7c1f5ce5926834bf96f606366b58ff58f715e2
afonse03 <amanda.m.fonseca03@gmail.com>
2021-11-20 08:37:29 -0700

    renamed branch user-profile-edit to amanda

--- src/main/java/com/codeup/runcmc/controllers/AlbumController.java
+++ src/main/java/com/codeup/runcmc/controllers/AlbumController.java
@@ -13,3 +13,4 @@ public class AlbumController {
         return "/album.html";
     }
 }
+//
--- src/main/java/com/codeup/runcmc/controllers/ProfileEditController.java
+++ src/main/java/com/codeup/runcmc/controllers/ProfileEditController.java
@@ -15,3 +15,4 @@ public class ProfileEditController {
         return "/profile-edit.html";
     }
 }
+//
