c915e0cd42759cadaef91bb4f0565b31ca0dd8fe
afonse03 <amanda.m.fonseca03@gmail.com>
2021-11-26 15:04:02 -0700

    added login page, login/logout func. working, troubleshoot exception at another time

--- src/main/java/com/codeup/runcmc/controllers/UserController.java
+++ src/main/java/com/codeup/runcmc/controllers/UserController.java
@@ -1,12 +1,25 @@
 package com.codeup.runcmc.controllers;
 
 import com.codeup.runcmc.models.User;
+import com.codeup.runcmc.repositories.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
+    //(**)
+    private UserRepository userDao;
+    private PasswordEncoder passwordEncoder;
+
+    public UserController(UserRepository userDao, PasswordEncoder passwordEncoder) {
+        this.userDao = userDao;
+        this.passwordEncoder = passwordEncoder;
+    }
+
     @GetMapping("/register")
     String showRegisterPage(Model model){
         model.addAttribute("user", new User());
@@ -16,5 +29,14 @@ public class UserController {
     @GetMapping("/profile")
 	String showProfile(){
     	return "user/profile.html";
+    }
+
+//    not sure if i need this for login/logout func. Refactor for our project
+    @PostMapping("/register")
+    public String saveUser(@ModelAttribute User user){
+        String hash = passwordEncoder.encode(user.getPassword());
+        user.setPassword(hash);
+        userDao.save(user);
+        return "redirect:/discover";
     }
 }
