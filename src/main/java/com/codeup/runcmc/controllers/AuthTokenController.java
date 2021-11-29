7955b98893fb65a23973f3e36f601a01d770a715
Sebastian McKelvey <sebastian.mckelvey@gmail.com>
2021-11-29 16:57:08 -0600

    Display selected album at /album/{id} and rename SpotifyController to AuthTokenController

--- src/main/java/com/codeup/runcmc/controllers/AuthTokenController.java
+++ src/main/java/com/codeup/runcmc/controllers/AuthTokenController.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class SpotifyTestController {
+public class AuthTokenController {
 
-    @GetMapping("/spotify")
+    @GetMapping("/auth")
     @ResponseBody
     public TokenResponse requestToken() {
         return RestTemplateTokenRequester.requestAccessToken();
