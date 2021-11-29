c28778efd3a85b6584a885af8d8622aa9fe18757
Ian Arellano <ian.g.arellano01@gmail.com>
2021-11-29 12:05:32 -0600

    finished topster partial, adjusted navbar to use discover instead of about, edited user profile to take in a user object, made profile require a login first

--- src/main/java/com/codeup/runcmc/controllers/UserController.java
+++ src/main/java/com/codeup/runcmc/controllers/UserController.java
@@ -2,6 +2,7 @@ package com.codeup.runcmc.controllers;
 
 import com.codeup.runcmc.models.User;
 import com.codeup.runcmc.repositories.UserRepository;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -27,7 +28,11 @@ public class UserController {
     }
 
     @GetMapping("/profile")
-	String showProfile(){
+	String showProfile(Model viewModel){
+	    User principal = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+	    User currentUser = userDao.getById(principal.getId());
+	    System.out.println(currentUser.getId());
+	    viewModel.addAttribute("user", currentUser);
     	return "user/profile.html";
     }
 
