24ec0e830634353dfb5a3c20562394ba92536c43
afonse03 <amanda.m.fonseca03@gmail.com>
2021-11-30 09:39:57 -0700

    working solution for user profile edit func.

--- src/main/java/com/codeup/runcmc/controllers/ProfileEditController.java
+++ src/main/java/com/codeup/runcmc/controllers/ProfileEditController.java
@@ -1,16 +1,39 @@
 package com.codeup.runcmc.controllers;
 
+import com.codeup.runcmc.models.User;
+import com.codeup.runcmc.repositories.UserRepository;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 // why is this page not loading correctly in the browser?
 
 @Controller
 public class ProfileEditController {
 
-    @GetMapping("/profile-edit")
-    public String profileEdit() {
+    private UserRepository userDao;
+
+    public ProfileEditController(UserRepository userDao) {
+        this.userDao = userDao;
+    }
+
+    @GetMapping("/profile-edit/{id}")
+    public String profileEdit(Model viewModel, @PathVariable long id) {
+        viewModel.addAttribute("user", userDao.getById(id));
         return "user/profile-edit.html";
+    }
+
+    @PostMapping("/profile-edit/{id}")
+    public String updateProfile(@ModelAttribute User user) {
+        User editedProfile = userDao.getById(user.getId());
+        editedProfile.setUsername(user.getUsername());
+        editedProfile.setPassword(user.getPassword());
+        editedProfile.setEmail(user.getEmail());
+        editedProfile.setBio(user.getBio());
+        System.out.println(user.getId());
+        System.out.println(user.getUsername());
+        userDao.save(editedProfile);
+        return "redirect:profile";
     }
 }
