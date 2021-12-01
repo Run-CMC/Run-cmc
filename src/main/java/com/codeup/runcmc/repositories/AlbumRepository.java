64547452394356017ad39384c261b4b50eda98a1
Ian Arellano <ian.g.arellano01@gmail.com>
2021-12-01 12:26:05 -0600

    started on implementing topster object. working on implementing topster partials. gonna ask teacher for roadblock assistance probably most likely

--- src/main/java/com/codeup/runcmc/repositories/AlbumRepository.java
+++ src/main/java/com/codeup/runcmc/repositories/AlbumRepository.java
@@ -3,7 +3,15 @@ package com.codeup.runcmc.repositories;
 import com.codeup.runcmc.models.Album;
 import com.codeup.runcmc.models.TopsterContent;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 public interface AlbumRepository extends JpaRepository <Album, Long> {
 
+	@Query(nativeQuery = true, value = "select * from albums join topster_contents tc on albums.id = tc.album_id join albums a on a.id = tc.album_id join topsters t on t.id = tc.topster_id where topster_id = :i")
+	List<Album> selectWithTopsterFK(@Param("i") long id);
 }
