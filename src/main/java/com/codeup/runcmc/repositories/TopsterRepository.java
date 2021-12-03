379c3b8b1aa72c4aa8bb3f7141cd21e0cda06c9c
Ian Arellano <ian.g.arellano01@gmail.com>
2021-12-03 16:46:52 -0600

    finished user profile to display topsters they made

--- src/main/java/com/codeup/runcmc/repositories/TopsterRepository.java
+++ src/main/java/com/codeup/runcmc/repositories/TopsterRepository.java
@@ -2,8 +2,14 @@ package com.codeup.runcmc.repositories;
 
 import com.codeup.runcmc.models.Topster;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface TopsterRepository extends JpaRepository<Topster, Long> {
     Topster findByTitle(String title);
     Topster getById(long id);
+	@Query(nativeQuery = true, value = "select * from topsters where user_id = :i")
+    List<Topster> findAllByUser(@Param("i") long id);
 }
