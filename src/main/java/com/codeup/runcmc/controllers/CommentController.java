c322086da49a8f4aac17a18f2e3efdcd8effb9b2
afonse03 <amanda.m.fonseca03@gmail.com>
2021-12-06 20:16:27 -0700

    added func. so user can delete their comment

--- src/main/java/com/codeup/runcmc/controllers/CommentController.java
+++ src/main/java/com/codeup/runcmc/controllers/CommentController.java
@@ -7,9 +7,7 @@ import com.codeup.runcmc.repositories.TopsterRepository;
 import com.codeup.runcmc.repositories.UserRepository;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
 
 
 @Controller
@@ -18,7 +16,6 @@ public class CommentController {
     private CommentRepository commentsDao;
     private TopsterRepository topsterDao;
     private UserRepository usersDao;
-
 
     public CommentController(CommentRepository commentsDao, TopsterRepository topsterDao, UserRepository usersDao) {
         this.commentsDao = commentsDao;
@@ -29,26 +26,18 @@ public class CommentController {
     @PostMapping("discover/topster/{id}")
     public String addComment(@PathVariable long id, @RequestParam(name = "body") String body){
         Comment comment = new Comment();
-
         User principal = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         User author = usersDao.getById(principal.getId());
         comment.setUser(author);
-
         comment.setBody(body);
         comment.setTopster(topsterDao.getById(id));
-
-
         commentsDao.save(comment);
         return "redirect:/discover/topster/" + id;
     }
 
-//    @GetMapping("discover/topster/{id}")
-//    public String showIndividualTopsterPage(Model model, @PathVariable long id) {
-//        model.addAttribute("topster",topsterRepository.getById(id));
-//        return "topster.html";
-//    }
-
-
-
-
+    @PostMapping("/discover/topster/{id}/delete")
+    public String deleteComment(@PathVariable long id, @ModelAttribute Comment comment) {
+        commentsDao.deleteById(comment.getId());
+        return "redirect:/discover/topster/" + id;
+    }
 }
