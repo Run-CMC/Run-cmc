d524a6d98431a4c728285367539a5e7fb9fe0224
Orenthal Hill <ojhill98@gmail.com>
2021-12-09 15:37:29 -0600

    progress on search

--- src/main/java/com/codeup/runcmc/controllers/SearchController.java
+++ src/main/java/com/codeup/runcmc/controllers/SearchController.java
@@ -7,6 +7,7 @@ import com.codeup.runcmc.repositories.UserRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -23,12 +24,14 @@ public class SearchController {
         this.topsterDao = topsterDao;
         this.albumDao = albumDao;
     }
-    @PostMapping ("/search")
-    public String searchPage(@RequestParam (name = "search-bar")String searchterms, Model model){
-     List<Topster> searchedTopsters = topsterDao.findAllByTitle(searchterms);
-     model.addAttribute("searchedtopsters",searchedTopsters);
-        return "redirect:/search";
+    @GetMapping("/search")
+    public String showSearchPage(@RequestParam (name = "search-bar", required = false)String searchterms, Model model){
+        List<Topster> searchedTopsters = topsterDao.findByTitleIgnoreCaseContaining(searchterms);
+        model.addAttribute("searchedTopsters",searchedTopsters);
+        for(Topster topster : searchedTopsters){
+            System.out.println(topster.getTitle());
+        }
+        return "search";
     }
-
 
 }
