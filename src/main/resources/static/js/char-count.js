4c9d34e90f7a71101d7c0bd8e257f21507822802
Christopher Lundblade <chris.lundblade@gmail.com>
2021-12-09 22:46:49 -0600

    incorporated char count on create topster and refactored, cleaned up and debugged drag-drop logic

--- src/main/resources/static/js/char-count.js
+++ src/main/resources/static/js/char-count.js
@@ -8,4 +8,13 @@ $(document).ready(function() {
 
 		$('#char-count').html(text_remaining + ' characters remaining');
 	});
+	//topster creation
+	$('#char-count').html(text_max + ' characters remaining');
+
+	$('#topster-text-body').keyup(function() {
+		var text_length = $('#topster-text-body').val().length;
+		var text_remaining = text_max - text_length;
+
+		$('#char-count').html(text_remaining + ' characters remaining');
+	});
 });
