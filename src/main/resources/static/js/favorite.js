85a40b49915af25ab5fdebf73820a163162d59e2
Christopher Lundblade <chris.lundblade@gmail.com>
2022-02-18 17:19:50 -0600

    adjusted favoriteClick function in js to increment or decrement the displayed number of favorites when a user clicks to favorite

--- src/main/resources/static/js/favorite.js
+++ src/main/resources/static/js/favorite.js
@@ -2,10 +2,8 @@
 
 function favoriteClick(e){
     let data = e.target.dataset.topsterid;
-    console.log(e.target.outerHTML);
-    let nextSmallElement = (e.target.parentNode.nextSibling);
-
-    // console.log(data);
+    console.log(e.target.parentNode.nextSibling.nextSibling.outerHTML);
+    let currentDisplayedNumberOfFavoritesOnTarget = parseInt((e.target.parentNode.nextSibling.nextSibling.innerText).substring(1));
     fetch(`/favorites/topster/${data}`,{
         method: 'POST',
         credentials: "same-origin",
@@ -21,9 +19,11 @@ function favoriteClick(e){
         if (e.target.classList.contains("fas")) {
             e.target.classList.remove("fas");
             e.target.classList.add("far");
+            (e.target.parentNode.nextSibling.nextSibling.innerText) = (e.target.parentNode.nextSibling.nextSibling.innerText).substring(0,1) + (currentDisplayedNumberOfFavoritesOnTarget-1);
         } else if (e.target.classList.contains("far")) {
             e.target.classList.remove("far");
             e.target.classList.add("fas");
+            (e.target.parentNode.nextSibling.nextSibling.innerText)= (e.target.parentNode.nextSibling.nextSibling.innerText).substring(0,1) + (currentDisplayedNumberOfFavoritesOnTarget+1);
         }
     })
         .catch(error => console.error(error));
